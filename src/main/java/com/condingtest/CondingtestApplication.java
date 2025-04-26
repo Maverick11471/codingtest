560a6d87e58a088933a82048078831655f631efc
youngwoo <dudaks0805@gmail.com>
2025-04-26 10:38:05 +0900

    feat: first commit

---
+++ src/main/java/com/condingtest/CondingtestApplication.java
@@ -0,0 +1,13 @@
+package com.condingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondingtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CondingtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/condingtest/CondingtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.condingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondingtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
