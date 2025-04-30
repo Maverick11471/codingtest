09cbd49bbaea711163baf5870deabadd09ab3c70
youngwoo <dudaks0805@gmail.com>
2025-04-30 10:12:22 +0900

    faet: baekjoon24479 solution

--- src/main/java/com/condingtest/Main.java
+++ src/main/java/com/condingtest/Main.java
@@ -3,38 +3,59 @@ package com.condingtest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
 public class Main {
+  static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+  static int[] visited;
+  static int order = 1;
+
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-    String str = br.readLine();
-    String bomb = br.readLine();
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+    int R = Integer.parseInt(st.nextToken());
 
-    StringBuilder sb = new StringBuilder();
+    visited = new int[N + 1];
 
-    int bombLength = bomb.length();
-    for (int i = 0; i < str.length(); i++) {
-      sb.append(str.charAt(i));
-
-      if (sb.length() >= bombLength) {
-        boolean flag = true;
-        for (int j = 0; j < bombLength; j++) {
-          if (sb.charAt(sb.length() - bombLength + j) != bomb.charAt(j)) {
-            flag = false;
-            break;
-          }
-        }
-        if (flag) {
-          sb.delete(sb.length() - bombLength, sb.length());
-        }
-      }
+    for (int i = 0; i <= N; i++) {
+      graph.add(new ArrayList<>());
     }
 
-    if (sb.length() == 0) {
-      System.out.println("FRULA");
-    } else {
-      System.out.println(sb.toString());
+    for (int i = 0; i < M; i++) {
+      st = new StringTokenizer(br.readLine());
+      int u = Integer.parseInt(st.nextToken());
+      int v = Integer.parseInt(st.nextToken());
+
+      graph.get(u).add(v);
+      graph.get(v).add(u);
+    }
+
+    for (int i = 1; i <= N; i++) {
+      Collections.sort(graph.get(i));
+    }
+
+    dfs(R);
+
+    StringBuilder sb = new StringBuilder();
+    for (int i = 1; i <= N; i++) {
+      sb.append(visited[i]).append('\n');
+    }
+
+    System.out.print(sb);
+  }
+
+  static void dfs(int node) {
+    visited[node] = order++;
+
+    for (int next : graph.get(node)) {
+      if (visited[next] == 0) {
+        dfs(next);
+      }
     }
   }
 }
