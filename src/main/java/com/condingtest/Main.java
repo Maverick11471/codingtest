001cc71e68fee84119e6857b9544c8b9f195443e
youngwoo <dudaks0805@gmail.com>
2025-05-01 14:40:21 +0900

    feat: baekjoon2252 solution

--- src/main/java/com/condingtest/Main.java
+++ src/main/java/com/condingtest/Main.java
@@ -4,23 +4,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-  static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-  static int[] visited;
-  static int order = 1;
-
   public static void main(String[] args) throws IOException {
+
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
 
     int N = Integer.parseInt(st.nextToken());
     int M = Integer.parseInt(st.nextToken());
-    int R = Integer.parseInt(st.nextToken());
 
-    visited = new int[N + 1];
+    List<List<Integer>> graph = new ArrayList<>();
+    int[] indegree = new int[N + 1];
 
     for (int i = 0; i <= N; i++) {
       graph.add(new ArrayList<>());
@@ -28,34 +27,33 @@ public class Main {
 
     for (int i = 0; i < M; i++) {
       st = new StringTokenizer(br.readLine());
-      int u = Integer.parseInt(st.nextToken());
-      int v = Integer.parseInt(st.nextToken());
+      int A = Integer.parseInt(st.nextToken());
+      int B = Integer.parseInt(st.nextToken());
 
-      graph.get(u).add(v);
-      graph.get(v).add(u);
+      graph.get(A).add(B);
+      indegree[B]++;
     }
 
+    Queue<Integer> queue = new LinkedList<>();
     for (int i = 1; i <= N; i++) {
-      Collections.sort(graph.get(i));
-    }
-
-    dfs(R);
-
-    StringBuilder sb = new StringBuilder();
-    for (int i = 1; i <= N; i++) {
-      sb.append(visited[i]).append('\n');
-    }
-
-    System.out.print(sb);
-  }
-
-  static void dfs(int node) {
-    visited[node] = order++;
-
-    for (int next : graph.get(node)) {
-      if (visited[next] == 0) {
-        dfs(next);
+      if (indegree[i] == 0) {
+        queue.offer(i);
       }
     }
+
+    StringBuilder sb = new StringBuilder();
+
+    while (!queue.isEmpty()) {
+      int now = queue.poll();
+      sb.append(now).append(" ");
+
+      for (int next : graph.get(now)) {
+        indegree[next]--;
+        if (indegree[next] == 0) {
+          queue.offer(next);
+        }
+      }
+    }
+    System.out.println(sb.toString());
   }
 }
